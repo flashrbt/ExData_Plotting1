5d812123be218ebcd8cb3ccb516876301555a894
flashrbt2015@gmail.com <flashrbt2015@gmail.com>
2016-10-06 13:10:15 -0700

    modified plot1.R, add format into as.Date() for subsetting newdata2

--- Plot1.R
+++ Plot1.R
@@ -8,7 +8,8 @@ file<-"household_power_consumption.txt"
 dt_data<-fread(file,na.strings = c("?"))
 
 newdata<-dt_data[grep("?/2/2007",dt_data$Date)]
-newdata2<-newdata[as.Date(newdata$Date)<as.Date("3/2/2007"),]
+
+newdata2<-newdata[as.Date(newdata$Date,format="%d/%m/%Y")<as.Date("3/2/2007",format="%d/%m/%Y"),]
 
 
 png(file="plot1.png",width=480,height=480,units='px')
